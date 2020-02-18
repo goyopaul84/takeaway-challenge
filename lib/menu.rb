52d7c298897e2e4ddc9455789c7ead5f3be0b7c4
justcallmegoyo <justcallmegoyo@gmail.com>
2020-02-18 12:00:06 +0000

    Menu returns one dish with price in customer viewable format

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,9 +1,18 @@
 class Menu
 
-attr_reader :dishes, :prices
+  attr_reader :dishes, :prices
 
   def initialize(dish, price)
     @dishes = [dish]
     @prices = [price]
+  end
+
+  def add_dish(dish, price)
+    @dishes.push(dish)
+    @prices.push(price)
+  end
+
+  def print_out
+    return "#{dishes[0]} : £#{prices[0]}"
   end
 end
