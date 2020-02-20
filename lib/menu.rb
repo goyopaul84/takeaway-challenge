9d723868b68dbd116ab821783fb1bf40ae1edbb9
justcallmegoyo <justcallmegoyo@gmail.com>
2020-02-20 14:26:02 +0000

    Restarted from zero

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,18 +1,2 @@
 class Menu
-
-  attr_reader :dishes, :prices
-
-  def initialize(dish, price)
-    @dishes = [dish]
-    @prices = [price]
-  end
-
-  def add_dish(dish, price)
-    @dishes.push(dish)
-    @prices.push(price)
-  end
-
-  def print_out
-    return "#{dishes[0]} : £#{prices[0]}"
-  end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,24 +3,7 @@ require 'menu'
 describe Menu do
 
   it 'exists' do
-    menu = Menu.new("dish", 5.95)
+    menu = Menu.new
     expect(menu).to be
-  end
-
-  it 'holds a single dish name and price' do
-    menu = Menu.new("Chicken", 4.79)
-    p menu
-    expect(menu).to have_attributes(:dishes => ["Chicken"], :prices => [4.79])
-  end
-
-  it 'can receive extra dishes and prices after initialisation' do
-    menu = Menu.new("Pizza", 3.5)
-    menu.add_dish("Lasagne", 5)
-    expect(menu.dishes).to contain_exactly("Pizza", "Lasagne")
-  end
-
-  it 'can return one dish with its price' do
-    menu = Menu.new("Curry", 3.25)
-    expect(menu.print_out).to eq("Curry : £3.25")
   end
 end
